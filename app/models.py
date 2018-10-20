0ab5a1370726de436a108bcae1b584cb27e248c6
fendrbendr87 <ymorozo@gmail.com>
2018-10-19 23:03:52 -0400

    whisky and coding

---
+++ app/models.py
@@ -0,0 +1,53 @@
+from app import db, login, app
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from time import time
+import jwt
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    Prospects=db.relationship('Prospects', backref='leads', lazy='dynamic')
+#    Clients=db.relationship('orders', backref='order_history', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash=generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+        {'reset_password': self.id, 'exp': time() + expires_in},
+        app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+    
+class Prospects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name=db.Column(db.String(20))
+    last_name=db.Column(db.String(20))
+    phone_cell=db.Column(db.Integer)
+    user_account_pk=db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Prospects> first_name: {}, last_name: {}, phone_cell: {}'.format(self.first_name, self.last_name, self.phone_cell)
+
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
