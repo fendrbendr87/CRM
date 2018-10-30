5b02d41da579a8c525c74424a56705907736955a
fendrbendr87 <ymorozo@gmail.com>
2018-10-29 23:02:32 -0400

    learned some charting / candlestick with pandas, put it in index for now as a placeholder

--- app/routes.py
+++ app/routes.py
@@ -6,13 +6,44 @@ from app.queryfunc import create_prospect, create_client, edit_prospect, get_pro
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-
+from pandas_datareader import data
+import datetime
+from bokeh.plotting import figure, show, output_file
+from bokeh.embed import components
+from bokeh.resources import CDN
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    return render_template("index.html", title='Index')
+    def inc_dec(c, o):
+        if c > o:
+            value="Increase"
+        elif c < o:
+            value="Decrease"
+        else:
+            value="Equal"
+        return value
+
+    start=datetime.datetime(2018,1,1)
+    end=datetime.datetime(2018,10,29)
+    df = data.DataReader(name="NFLX", data_source="yahoo", start=start, end=end)
+    df["Status"]=[inc_dec(c,o) for c, o in zip(df.Close, df.Open)]
+    df["Middle"]=(df.Open+df.Close)/2
+    df["Height"]=abs(df.Close-df.Open)
+    p=figure(title="Candelstick Chart", x_axis_type='datetime', width=1000, height=300)
+    p.grid.grid_line_alpha=0.3
+    hours_12=12*60*60*1000
+    p.segment(df.index, df.High, df.index, df.Low, color="Black")
+    p.rect(df.index[df.Status=="Increase"], df.Middle[df.Status=="Increase"],
+                hours_12, df.Height[df.Status=="Increase"], fill_color="#CCFFFF", line_color="black")
+    p.rect(df.index[df.Status=="Decrease"], df.Middle[df.Status=="Decrease"],
+                hours_12, df.Height[df.Status=="Decrease"], fill_color="#FF3333", line_color="black")
+
+    script1, div1 = components(p)
+    cdn_js=CDN.js_files
+    cdn_css=CDN.css_files
+    return render_template("index.html", title='Index', script1=script1, div1=div1, cdn_js=cdn_js[0], cdn_css=cdn_css[0])
 
 @app.route('/listings', methods=['GET', 'POST'])
 @login_required
